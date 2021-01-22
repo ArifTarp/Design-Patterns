90591cb92b2ff16305eda6585c7c6e8333b8e449
ArifTarp <arifoklh@gmail.com>
2021-01-22 17:43:16 +0300

    Added singleton design pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Message();
+
+            Console.ReadKey();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+        private static object _lockObject = new object();
+
+        // barrier to outside access with private constructor
+        private CustomerManager()
+        {
+            
+        }
+
+        // If the instance of CustomerManager already exists we will give the existing one
+        // otherwise we will create a new one and give
+        public static CustomerManager CreateAsSingleton()
+        {
+            // multithread protection with lock
+            lock (_lockObject)
+            {
+                if (_customerManager == null)
+                {
+                    _customerManager = new CustomerManager();
+                }
+            }
+            return _customerManager;
+        }
+
+        public void Message()
+        {
+            Console.WriteLine("Created CustomerManager instance or gave the existing one");
+        }
+    }
+}
