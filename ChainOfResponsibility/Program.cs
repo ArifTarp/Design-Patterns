ce8571ebbf17fe6ad00b22547f2d5e2df640e586
ArifTarp <arifoklh@gmail.com>
2021-02-04 18:34:31 +0300

    Added chain of responsibility design pattern

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpenseHandlerBase president = new President();
+            ExpenseHandlerBase manager = new Manager();
+            ExpenseHandlerBase vicePresident = new VicePresident();
+
+            vicePresident.SetSuccessor(president);
+            manager.SetSuccessor(vicePresident);
+
+            Expense expense = new Expense{Detail = "Training", Amount = 1500};
+            manager.HandleExpense(expense);
+
+            Console.ReadLine();
+        }
+    }
+
+    class President : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 1000)
+            {
+                Console.WriteLine("President Handled Expense!");
+            }
+        }
+    }
+
+    class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 100 && expense.Amount <= 1000)
+            {
+                Console.WriteLine("Vice President Handled Expense!");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    class Manager : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount <= 100)
+            {
+                Console.WriteLine("Manager Handled Expense!");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase Successor;
+        public abstract void HandleExpense(Expense expense);
+
+        public void SetSuccessor(ExpenseHandlerBase successor)
+        {
+            Successor = successor;
+        }
+    }
+
+    class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
