a0d7a7e5ce2b9073a88c7d625ddca0144fe3358f
ArifTarp <arifoklh@gmail.com>
2021-02-04 14:34:13 +0300

    Added strategy design pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CustomerDbOperations = new ADataBase();
+            customerManager.SaveCustomer();
+
+            customerManager.CustomerDbOperations = new BDataBase();
+            customerManager.SaveCustomer();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public CustomerDbOperations CustomerDbOperations { get; set; }
+        public void SaveCustomer()
+        {
+            CustomerDbOperations.Add();
+            Console.WriteLine("Customer Added");
+        }
+    }
+
+    class ADataBase : CustomerDbOperations
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Customer add to A database");
+        }
+    }
+
+    class BDataBase : CustomerDbOperations
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Customer add to B database");
+        }
+    }
+
+    abstract class CustomerDbOperations
+    {
+        public abstract void Add();
+    }
+}
