b772b4606835b1b64db0a5d219a1170c2875588d
ArifTarp <arifoklh@gmail.com>
2021-02-05 15:15:27 +0300

    Added template method design pattern

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserAlgorithm algorithm;
+
+            algorithm = new AddCustomerAlgorithm();
+            algorithm.AddUser(new { }, "A Server");
+
+            Console.WriteLine("\n ********************** \n");
+
+            algorithm = new AddEmployeeAlgorithm();
+            algorithm.AddUser(new { }, "B Server");
+
+            Console.ReadLine();
+        }
+    }
+
+    class AddEmployeeAlgorithm : UserAlgorithm
+    {
+        protected override void AddUserToSource(object user, string source)
+        {
+            Console.WriteLine("Added employee user to " + source);
+        }
+
+        protected override void SendMessage(object user)
+        {
+            Console.WriteLine("Sent message for employee user with E-Mail");
+        }
+    }
+
+    class AddCustomerAlgorithm : UserAlgorithm
+    {
+        protected override void AddUserToSource(object user, string source)
+        {
+            Console.WriteLine("Added customer user to " + source);
+        }
+
+        protected override void SendMessage(object user)
+        {
+            Console.WriteLine("Sent message for customer user with SMS");
+        }
+    }
+
+    abstract class UserAlgorithm
+    {
+        // template method
+        public void AddUser(object user, string source)
+        {
+            AddUserToSource(user, source);
+            LogUser(user);
+            SendMessage(user);
+        }
+
+        protected abstract void AddUserToSource(object user, string source);
+
+        private void LogUser(object user)
+        {
+            Console.WriteLine("Logged user");
+        }
+
+        protected abstract void SendMessage(object user);
+    }
+}
